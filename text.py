849a9843135e0f0e081f3f8592dd1f87c77cdbda
Thenuja <dulwanat@gmail.com>
2023-10-06 22:37:12 +0530

    Update Code

---
+++ text.py
@@ -0,0 +1,12 @@
+
+#take the user's name as input
+#the vaiable that includes the welcome message is alredy provided.
+#Please complete this part using the knowledge obtained in this lesson.
+#greeting = ("Welcome "+ name)
+
+#print the welcome message
+#print(greeting)
+
+a= "fhffgrg"+"hdwh"
+a
+
