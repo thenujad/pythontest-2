5c6cd54b7d3aec927e209334057c37f95199fdf8
Thenuja <dulwanat@gmail.com>
2023-10-06 22:54:34 +0530

    Updated

--- text.py
+++ text.py
@@ -1,12 +1,20 @@
+#print("hello world")
+
+#print(2 * 3 ** 3 * 2)
+
+#print(27*4)
+
 
 #take the user's name as input
+name = input("Enter your name: ")
+print(name)
 #the vaiable that includes the welcome message is alredy provided.
 #Please complete this part using the knowledge obtained in this lesson.
-#greeting = ("Welcome "+ name)
+greeting = ("Welcome "+ name)
 
 #print the welcome message
-#print(greeting)
+print(greeting)
 
-a= "fhffgrg"+"hdwh"
-a
+a  = "fhffgrg"+"hdwh"
 
+type(a)
